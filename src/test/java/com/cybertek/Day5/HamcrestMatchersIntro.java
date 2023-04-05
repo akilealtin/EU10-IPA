5b9f17bf0ea3c679e4ef29f8da28e5fe8f307a04
Akile Altin <altinakile10@gmail.com>
2023-04-05 16:40:55 +0300

    created java number examples

---
+++ src/test/java/com/cybertek/Day5/HamcrestMatchersIntro.java
@@ -0,0 +1,41 @@
+package com.cybertek.Day5;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+public class HamcrestMatchersIntro {
+    @Test
+    public void simpleTest(){
+        //actual 5+5
+        assertThat(5+5, is(10));
+        assertThat(5+5,equalTo(10));
+        //matchers has 2 overloaded version
+        //first that accept actual value
+        //second taht accept another matchers
+        //below examples is method is accepting another matchers equal to make it readable
+        assertThat(5+5,is(equalTo(10)));
+
+        assertThat(5+5,not(9));
+        assertThat(5+5,is(not(9)));
+        assertThat(5+5,is(not(equalTo(9))));
+
+        //number comparison
+        //greaterThan()
+        //greaterThanOrEqualTo()
+        //lessThan()
+        //lessThanOrEqualTo()
+        assertThat(5+5,is(greaterThan(9)));
+
+    }
+
+
+}
