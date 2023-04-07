57351318ebb41401418945f93861e538cb1b4eb0
Akile Altin <altinakile10@gmail.com>
2023-04-07 16:45:12 +0300

    created  classes

---
+++ src/test/java/com/cybertek/Day5/SpartanHamcrestTest.java
@@ -0,0 +1,62 @@
+package com.cybertek.Day5;
+import com.cybertek.utilities.*;
+import io.restassured.http.*;
+import org.junit.jupiter.api.*;
+
+import java.util.*;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import com.cybertek.utilities.SpartanTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SpartanHamcrestTest extends SpartanTestBase {
+    @DisplayName("GET spartan/search and chaining together")
+    @Test
+    public void test1() {
+        //along with this statement, I want to save names inside the List<String>
+
+        List<String> names = given().accept(ContentType.JSON)
+                .and()
+                .queryParams("nameContains","j",
+                        "gender","Male")
+                .when()
+                .get("/api/spartans/search")
+                .then()
+                .statusCode(200)
+                .and()
+                .body("totalElement",greaterThanOrEqualTo(3))
+                .extract().response().jsonPath().getList("content.name");
+
+        System.out.println(names);
+
+    }
+
+    @DisplayName("GET spartan/search and chaining together")
+    @Test
+    public void test2(){
+
+        //save status code
+
+        int statusCode = given().accept(ContentType.JSON)
+                .and()
+                .queryParams("nameContains","j",
+                        "gender","Male")
+                .when()
+                .get("/api/spartans/search")
+                .then()
+                .statusCode(200)
+                .and()
+                .body("totalElement",greaterThanOrEqualTo(3))
+                .extract().response().statusCode();
+
+        System.out.println(statusCode);
+
+
+
+    }
+
+
+}
